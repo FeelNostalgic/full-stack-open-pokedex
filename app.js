f4c0eba8e9e72d778dbf5bb3adad18d7b829cc5a
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-05-13 13:38:45 +0200

    Updated version

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1.0.0')
+  res.send('1.1.0')
 })
 
 app.get('/health', (req, res) => {
